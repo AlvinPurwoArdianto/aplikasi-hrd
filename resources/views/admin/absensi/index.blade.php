9ca121a4424381878ccf3b2158d5c262caf08a43
alvin <alvinpurwo03@gmail.com>
2024-10-23 00:48:31 +0700

    update absensi, cuti, laporan, penggajian, rekrutmen, pegawai

--- resources/views/admin/absensi/index.blade.php
+++ resources/views/admin/absensi/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container-xxl flex-grow-1 container-p-y">
-        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Employee /</span> Absensi</h4>
+        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Menu Absensi /</span> Absensi</h4>
 
         <!-- Toast Untuk Success -->
         @if (session('success'))
